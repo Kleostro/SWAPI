2f69767af484b78b9d97799e7bb00b93d7ae4877
Max <133934232+Kleostro@users.noreply.github.com>
2023-10-28 20:21:41 +0300

    fix: removed the need for asynchronous page loading

--- film-details.js
+++ film-details.js
@@ -1,7 +1,7 @@
 import { translateArabicToRomanNumber } from './utilits.js';
 import { renderPage, loadResource } from './main.js';
 
-export const render = async (data) => {
+export const render = (data) => {
   console.log("Я детальная страница фильма", data);
 
   const episodeNumber = translateArabicToRomanNumber(data.episode_id);
@@ -20,10 +20,10 @@ export const render = async (data) => {
   backToLink.classList.add("btn", "btn-primary");
   backToLink.textContent = "Back to episodes";
 
-  backToLink.addEventListener("click", async (e) => {
+  backToLink.addEventListener("click", (e) => {
     e.preventDefault();
     history.back();
-    await renderPage(
+    renderPage(
       "./films-list.js",
       "https://swapi.dev/api/films",
       "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
@@ -34,56 +34,77 @@ export const render = async (data) => {
   const speciesPromise = Promise.all(data.species.map(url => loadResource(url)));
   const starshipsPromise = Promise.all(data.starships.map(url => loadResource(url)));
 
-  const [planets, species, starships] = await Promise.all([planetsPromise, speciesPromise, starshipsPromise]);
-
   const planetsWrapper = document.createElement('div');
   planetsWrapper.classList.add('col');
 
-  const ulPlanets = document.createElement("ul");
-  ulPlanets.classList.add("planets-list");
-  planets.forEach((planet) => {
-    const li = document.createElement("li");
-    li.textContent = planet.name;
-    ulPlanets.append(li);
-  });
+  const preloader = document.createElement('p');
+  preloader.textContent = 'Загрузка...';
 
   const planetsTitle = document.createElement('h2');
   planetsTitle.textContent = 'Planets';
 
-  planetsWrapper.append(planetsTitle, ulPlanets);
+  const ulPlanets = document.createElement("ul");
+  ulPlanets.classList.add("planets-list");
+
+  planetsPromise.then((planets) => {
+    const planetsElems = planets.map((planet) => {
+      const li = document.createElement("li");
+      li.textContent = planet.name;
+      return li;
+    });
+    preloader.remove()
+    ulPlanets.append(...planetsElems);
+  });
+
+  planetsWrapper.append(planetsTitle, preloader, ulPlanets);
 
 
   const speciesWrapper = document.createElement('div');
   speciesWrapper.classList.add('col');
 
-  const ulSpecies = document.createElement("ul");
-  ulSpecies.classList.add("species-list");
-  species.forEach((specie) => {
-    const li = document.createElement("li");
-    li.textContent = specie.name;
-    ulSpecies.append(li);
-  });
-
   const speciesTitle = document.createElement('h2');
   speciesTitle.textContent = 'Species';
 
-  speciesWrapper.append(speciesTitle, ulSpecies);
+  const preloader2 = preloader.cloneNode(true);
+
+  const ulSpecies = document.createElement("ul");
+  ulSpecies.classList.add("species-list");
+
+  speciesPromise.then((species) => {
+    const speciesElems = species.map((specie) => {
+      const li = document.createElement("li");
+      li.textContent = specie.name;
+      return li;
+    });
+    preloader2.remove()
+    ulSpecies.append(...speciesElems);
+  });
+
+  speciesWrapper.append(speciesTitle, preloader2, ulSpecies);
+
 
   const starshipsWrapper = document.createElement('div');
   starshipsWrapper.classList.add('col');
 
-  const ulStarships = document.createElement("ul");
-  ulStarships.classList.add("starships-list");
-  starships.forEach((starship) => {
-    const li = document.createElement("li");
-    li.textContent = starship.name;
-    ulStarships.append(li);
-  });
-
   const starshipsTitle = document.createElement('h2');
   starshipsTitle.textContent = 'Starships';
 
-  starshipsWrapper.append(starshipsTitle, ulStarships);
+  const preloader3 = preloader.cloneNode(true);
+
+  const ulStarships = document.createElement("ul");
+  ulStarships.classList.add("starships-list");
+
+  starshipsPromise.then((starships) => {
+    const starshipsElems = starships.map((starship) => {
+      const li = document.createElement("li");
+      li.textContent = starship.name;
+      return li;
+    });
+    preloader3.remove()
+    ulStarships.append(...starshipsElems);
+  });
+
+  starshipsWrapper.append(starshipsTitle, preloader3, ulStarships);
 
   const detailsWrapper = document.createElement('div');
   detailsWrapper.classList.add('container', 'row');
